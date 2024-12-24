9012c791d1f944a305dca26524faf7ee86d77ea5
Sipurit <gun0820303553@gmail.com>
2024-12-24 15:36:54 +0700

    #

--- lab12_4.cpp
+++ lab12_4.cpp
@@ -2,6 +2,11 @@
 
 using namespace std;
 
+
+template<typename T>
+
+void mySwap(T &,T &); 
+
 int main(){
 	int x, y;
 	string a, b;
@@ -29,4 +34,14 @@ int main(){
 
 	return 0;
 }
+
+template<typename T>
+void mySwap(T &x, T &y){
+	T z = x;
+	x = y;
+	y = z;
+}
+
+
+
 
